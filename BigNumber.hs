b5526e519039722f102d682d17120d65d2be6463
André Flores <andrejesusferflores@gmail.com>
2021-11-21 20:23:26 +0000

    div done.

--- BigNumber.hs
+++ BigNumber.hs
@@ -10,6 +10,12 @@ isNegative (BN xs x) = isNegative xs
 isPositive :: BigNumber -> Bool
 isPositive = not . isNegative
 
+zeroBN :: BigNumber
+zeroBN = BN Empty 0
+
+oneBN :: BigNumber
+oneBN = BN Empty 1
+
 minusBNHelper :: BigNumber -> BigNumber
 minusBNHelper (BN Empty x) = BN Empty (-x)
 minusBNHelper (BN xs x) = BN (minusBNHelper xs) x
@@ -33,7 +39,7 @@ tailBN (BN xs x) = BN (tailBN xs) x
 
 paddBN :: BigNumber -> BigNumber -> BigNumber
 paddBN (BN Empty x) (BN Empty _) = BN Empty x
-paddBN (BN Empty x) (BN ys y) = BN (paddBN (BN Empty 0) ys) x
+paddBN (BN Empty x) (BN ys y) = BN (paddBN zeroBN ys) x
 paddBN (BN xs x) (BN ys y) = BN (paddBN xs ys) x
 
 scannerHelper :: String  -> BigNumber
@@ -41,7 +47,7 @@ scannerHelper [] = Empty
 scannerHelper (x:xs) = BN (scannerHelper xs) (digitToInt x)
 
 scanner :: String -> BigNumber
-scanner str 
+scanner str
     | head str /= '-' = scannerHelper (reverse str)
     | otherwise = minusBN (scannerHelper (reverse (tail str)))
 
@@ -55,8 +61,8 @@ somaPosBN :: BigNumber -> BigNumber -> BigNumber
 somaPosBN (BN Empty x) (BN Empty y)
     | x + y < 10 = BN Empty (x + y)
     | otherwise = BN (BN Empty ((x + y) `div` 10)) ((x + y) `mod` 10)
-somaPosBN x (BN Empty y) = somaPosBN x (BN (BN Empty 0) y)
-somaPosBN (BN Empty x) y = somaPosBN (BN (BN Empty 0) x) y
+somaPosBN x (BN Empty y) = somaPosBN x (BN zeroBN y)
+somaPosBN (BN Empty x) y = somaPosBN (BN zeroBN x) y
 somaPosBN (BN xs x) (BN ys y)
     | x + y < 10 = BN (somaPosBN xs ys) (x + y)
     | otherwise = BN (somaPosBN (BN headBN (nextBN + ((x + y) `div` 10))) ys) ((x + y) `mod` 10)
@@ -73,8 +79,8 @@ orderedSubPosBN :: BigNumber -> BigNumber -> BigNumber
 orderedSubPosBN (BN Empty x) (BN Empty y)
     | abs (x - y) < 10 = BN Empty (x - y)
     | otherwise = BN (BN Empty ((x - y) `div` 10)) ((x - y) `mod` 10)
-orderedSubPosBN x (BN Empty y) = orderedSubPosBN x (BN (BN Empty 0) y)
-orderedSubPosBN (BN Empty x) y = orderedSubPosBN (BN (BN Empty 0) x) y
+orderedSubPosBN x (BN Empty y) = orderedSubPosBN x (BN zeroBN y)
+orderedSubPosBN (BN Empty x) y = orderedSubPosBN (BN zeroBN x) y
 orderedSubPosBN (BN xs x) (BN ys y)
     | x >= y = BN (orderedSubPosBN xs ys) (x - y)
     | x < y = BN (orderedSubPosBN (BN headBN (nextBN - 1)) ys) (10 + x - y)
@@ -84,22 +90,22 @@ lengthBN :: BigNumber -> Int
 lengthBN Empty = 0
 lengthBN (BN xs x) = 1 + lengthBN xs
 
-isGreaterEqualLength :: BigNumber -> BigNumber -> Bool
-isGreaterEqualLength Empty Empty = False 
-isGreaterEqualLength x y
+greaterEqualLengthBN :: BigNumber -> BigNumber -> Bool
+greaterEqualLengthBN Empty Empty = False
+greaterEqualLengthBN x y
     | firstBN x > firstBN y = True
     | firstBN x < firstBN y = False
-    | otherwise = isGreaterEqualLength (tailBN x) (tailBN y)
+    | otherwise = greaterEqualLengthBN (tailBN x) (tailBN y)
 
-isGreater :: BigNumber -> BigNumber -> Bool
-isGreater x y
+greaterBN :: BigNumber -> BigNumber -> Bool
+greaterBN x y
     | lengthBN x < lengthBN y = False
-    | lengthBN x == lengthBN y = isGreaterEqualLength x y
-    | lengthBN x > lengthBN y = isGreaterEqualLength x (paddBN y x)
+    | lengthBN x == lengthBN y = greaterEqualLengthBN x y
+    | lengthBN x > lengthBN y = greaterEqualLengthBN x (paddBN y x)
 
 subBNHelper :: BigNumber -> BigNumber -> BigNumber
 subBNHelper x y
-    | isGreater y x = minusBN (orderedSubPosBN y x)
+    | greaterBN y x = minusBN (orderedSubPosBN y x)
     | otherwise = orderedSubPosBN x y
 
 subBN :: BigNumber -> BigNumber -> BigNumber
@@ -113,24 +119,49 @@ eBN :: BigNumber -> Int -> BigNumber
 eBN x 0 = x;
 eBN x n = BN (eBN x (n - 1)) 0
 
-baseMultBN :: BigNumber -> BigNumber -> Int -> Int -> BigNumber
-baseMultBN (BN Empty x) (BN Empty y) ex ey
+baseMulBN :: BigNumber -> BigNumber -> Int -> Int -> BigNumber
+baseMulBN (BN Empty x) (BN Empty y) ex ey
     | x * y < 10 = BN Empty (x * y) `eBN` (ex + ey)
     | otherwise = BN (BN Empty ((x * y) `div` 10)) ((x * y) `mod` 10) `eBN` (ex + ey)
-    
+
 digitOrdBN :: BigNumber -> Int -> BigNumber
 digitOrdBN (BN _ x) 0 = BN Empty x
 digitOrdBN (BN xs _) n = digitOrdBN xs (n - 1)
 
-multListBN :: BigNumber -> BigNumber -> [BigNumber]
-multListBN x y = [baseMultBN (digitOrdBN x xn) (digitOrdBN y yn) xn yn | xn <- [0 .. lengthBN x - 1], yn <- [0 .. lengthBN y - 1]]
+mulListBN :: BigNumber -> BigNumber -> [BigNumber]
+mulListBN x y = [baseMulBN (digitOrdBN x xn) (digitOrdBN y yn) xn yn | xn <- [0 .. lengthBN x - 1], yn <- [0 .. lengthBN y - 1]]
 
-multPosBN :: BigNumber -> BigNumber -> BigNumber
-multPosBN x y = foldl somaBN (BN Empty 0) (multListBN x y)
+mulPosBN :: BigNumber -> BigNumber -> BigNumber
+mulPosBN x y = foldl somaBN zeroBN (mulListBN x y)
 
 mulBN :: BigNumber -> BigNumber -> BigNumber
 mulBN x y
-    | isPositive x && isPositive y = multPosBN x y
-    | isPositive x && isNegative y = minusBN (multPosBN x (minusBN y))
-    | isNegative x && isPositive y = minusBN (multPosBN (minusBN x) y)
-    | otherwise  = multPosBN (minusBN x) (minusBN y)
+    | isPositive x && isPositive y = mulPosBN x y
+    | isPositive x && isNegative y = minusBN (mulPosBN x (minusBN y))
+    | isNegative x && isPositive y = minusBN (mulPosBN (minusBN x) y)
+    | otherwise  = mulPosBN (minusBN x) (minusBN y)
+
+equalsBN :: BigNumber -> BigNumber -> Bool
+equalsBN Empty Empty = True;
+equalsBN (BN xs x) (BN ys y)
+    | x /= y = False
+    | otherwise = equalsBN xs ys
+
+greaterOrEqualsBN :: BigNumber -> BigNumber -> Bool
+greaterOrEqualsBN x y = x `greaterBN` y || x `equalsBN` y
+
+lesserOrEqualsBN :: BigNumber -> BigNumber -> Bool
+lesserOrEqualsBN x y = not (x `greaterBN` y) || x `equalsBN` y
+
+baseDivBN :: BigNumber -> BigNumber -> BigNumber -> (BigNumber, BigNumber)
+baseDivBN x y q
+    | mulBN y q `greaterBN` x = (subBN q oneBN, subBN x (mulBN y (subBN q oneBN)))
+    | otherwise = baseDivBN x y (somaBN q oneBN) 
+
+divBNHelper :: BigNumber -> BigNumber -> BigNumber -> BigNumber -> (BigNumber, BigNumber)
+divBNHelper Empty _ q r = (q, r)
+divBNHelper x y q r = divBNHelper (tailBN x) y (removeLeadingZeros (BN q q_div)) (removeLeadingZeros r_div) 
+    where (BN _ q_div, r_div) = baseDivBN (removeLeadingZeros (BN r (firstBN x))) y oneBN
+
+divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
+divBN x y = divBNHelper x y zeroBN zeroBN
